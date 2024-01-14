74eaff91b0f5976f3d440184f63758353f5cf254
qRe <melsenmalina@gmail.com>
2024-01-14 17:06:18 +0300

    2 sorting algs

---
+++ Sorting_algorithms/Bubble_sort/main.go
@@ -0,0 +1,29 @@
+// Stable: Yes
+// Best Case: O(n^2)
+// Worst Case: O(n^2)
+// Average Case: O(n^2)
+// Space Complexity: O(1)
+
+package main
+
+import (
+	"fmt"
+)
+
+func bubbleSort(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr)-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+}
+
+func main() {
+	a := []int{1, 5, 3, 2, 4}
+
+	bubbleSort(a)
+
+	fmt.Printf("Sorted array: %v\n", a)
+}
---
+++ Sorting_algorithms/Insertion_sort/main.go
@@ -0,0 +1,34 @@
+// Stable: Yes
+// Best Case: O(n)
+// Worst Case: O(n^2)
+// Average Case: O(n^2)
+// Space Complexity: O(1)
+
+package main
+
+import (
+	"fmt"
+)
+
+func insertionSort(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		key := arr[i]
+		j := i - 1
+
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+
+			j--
+		}
+
+		arr[j+1] = key
+	}
+}
+
+func main() {
+	a := []int{1, 5, 3, 2, 4}
+
+	insertionSort(a)
+
+	fmt.Printf("Sorted array: %v\n", a)
+}
